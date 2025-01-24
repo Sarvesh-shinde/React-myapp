602b0373db6bda77fbfbe3a55edfc1ad9c872b1e
SARVESH <sarveshshinde2152@gmail.com>
2025-01-24 15:17:33 +0530

    Day eight

--- my-app1/src/App.jsx
+++ my-app1/src/App.jsx
@@ -1,36 +1,15 @@
 
 
-import Home from './components/Home'
-import About from './components/About'
-import Login from './components/Login'
-import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
-import './App.css';
-
-function Header(){
-  return(
-    <div>
-   
-    <ul>
-    <h1>Vasantdada Patil College Of Engineering</h1>
-      <li><Link to="/home">Home</Link></li>
-      <li><Link to="/about">About</Link></li>
-      <li><Link to="/login">Login</Link></li>
-    </ul>
-    </div>
-  )
-}
-
+import { useEffect } from "react"
 function App() { 
+  useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/users").then(
+      response=>response.json()
+    ).then(json=>console.log(json))
+   },[])
     return (
     <div>     
-      <BrowserRouter>
-      <Header/>
-      <Routes>
-      <Route path="/home" element={<Home/>}></Route>
-      <Route path="/about" element={<About/>}></Route>
-      <Route path="/login" element={<Login/>}></Route>
-      </Routes>
-      </BrowserRouter>
+     
     </div>
   )
 }
